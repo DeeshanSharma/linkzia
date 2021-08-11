65d0013a445e80da1b7ba7d083664536ae02a8c2
Deeshan Sharma <deeshansharma04@gmail.com>
2021-08-11 10:51:31 +0530

    ✨ feat: connect database and setup routes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+import connectDB from './config/db';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use('/', routes);
+
+connectDB();
 
 const PORT: number = parseInt(process.env.PORT as string, 10) || 5000;
 
-app.get('/', (req, res) => res.send('Server is up and running'));
 app.listen(PORT, () =>
   console.log(`⚡️[SERVER]: Server is running at http://localhost:${PORT}`)
 );
